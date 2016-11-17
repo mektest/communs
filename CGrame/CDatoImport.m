14614b8a465ed858c88db87f00ae116d8a950987
Marcel Etienne Kaszap <lemek22@gmail.com>
2016-11-17 14:39:23 -0500

    Update CDatoImport.m

--- CGrame/CDatoImport.m
+++ CGrame/CDatoImport.m
@@ -2,19 +2,19 @@
 % MEK - juin 2011
 %
 % Classe CDatoImport
-% pour faire les importation d'un fichier Analyse à un autre
+% pour faire les importation d'un fichier Analyse Ã  un autre
 %
 classdef CDatoImport < handle
   properties
-    debcan;           % donne la concordance du premier canal importé
-    fincan;           % donne ...           ... dernier ...
-    modcan;           % Nb de canaux ajouté, sinon < 0
-    debess;           % donne la concordance du premier essai importé
-    finess;           % donne ...           ... dernier ...
-    modess;           % Nb d'essai ajouté, sinon < 0
-    f0nad;            % donne le nombre de canal du fichier actif avant modif
-    f1nad;            % donne ...                       ... importé ...
-    f0ess;            % donne le nombre d'essai du fichier actif avant modif
-    f1ess;            % donne ...                      ... importé ...
+    debcan =[];           % donne la concordance du premier canal importÃ©
+    fincan =[];           % donne ...           ... dernier ...
+    modcan =[];           % Nb de canaux ajoutÃ©, sinon < 0
+    debess =[];           % donne la concordance du premier essai importÃ©
+    finess =[];           % donne ...           ... dernier ...
+    modess =[];           % Nb d'essai ajoutÃ©, sinon < 0
+    f0nad =[];            % donne le nombre de canal du fichier actif avant modif
+    f1nad =[];            % donne ...                       ... importÃ© ...
+    f0ess =[];            % donne le nombre d'essai du fichier actif avant modif
+    f1ess =[];            % donne ...                      ... importÃ© ...
   end
 end
