a32d539dd1793369726255a79fedfa40621b3c16
Marcel Etienne Kaszap <lemek22@gmail.com>
2016-11-17 15:40:48 -0500

    Update CFichEnum.m

--- CGrame/CFichEnum.m
+++ CGrame/CFichEnum.m
@@ -1,3 +1,13 @@
+%
+% Cette énumération contient les type de fichier qu'Analyse pourra ouvrir/convertir.
+%
+% Le nom doit rester exactement comme il est écrit ici car nous utilisons ce nom pour
+% former le nom de la fonction qui sera appelé pour lire le fichier voulu.
+%
+% ex: pour un fichier texte, la classe s'appelle:  CLireTexte
+%                     emg                       :  CLireEMG
+%
+
 classdef CFichEnum < uint16
   enumeration
     analyse   (1)
