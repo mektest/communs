f23b8df91c47793f3975a64065cf9e1fb7b08eae
Marcel Etienne Kaszap <lemek22@gmail.com>
2016-11-18 12:22:41 -0500

    Update CFinfo.m

--- CGrame/CFinfo.m
+++ CGrame/CFinfo.m
@@ -1,20 +1,25 @@
 %
 % Classe CFinfo
 %
+% Informations sur le fichier lui même
+%
+
 classdef CFinfo < handle
   properties
-    prenom ='';   % dossier de travail
-    finame ='';
-    foname ='';
-    fich;
-    fitmp;
-  end  %properties
-  %
+    prenom ='';   % dossier de travail (path)
+    finame ='';   % nom du fichier en entrée
+    foname ='';   % nom du fichier en sortie...appelé à disparaître
+    fich =[];     % est appelé à disparaître
+    fitmp =[];    % nom du fichier temporaire de travail
+  end
+
   methods
+
     function delete(obj)              % DESTRUCTOR
-      if ~isempty(obj.fitmp) & ~isempty(dir(obj.fitmp))
+      if ~isempty(obj.fitmp) && ~isempty(dir(obj.fitmp))
     	  delete(obj.fitmp);
       end
     end
-  end
-end
+
+  end % method
+end % classdef
