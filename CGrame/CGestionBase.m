37714da954fa7348259177331ca12bc1978e8b28
Marcel Etienne Kaszap <lemek22@gmail.com>
2016-11-18 12:28:37 -0500

    Update CGestionBase.m

--- CGrame/CGestionBase.m
+++ CGrame/CGestionBase.m
@@ -7,42 +7,49 @@
 %          initial(tO, V)
 %          ResetProp(tO)
 %
-%  Contient toutes les méthodes de bases pour les classes
-%  qui ont des propriétés qui doivent être sauvegarder.
-%  **** la propriété "monnom" doit être initialisé ****
+%  Contient toutes les mÃ©thodes de bases pour les classes
+%  qui ont des propriÃ©tÃ©s qui doivent Ãªtre sauvegarder.
+%  **** la propriÃ©tÃ© "monnom" doit Ãªtre initialisÃ© ****
 %
+
 classdef CGestionBase < handle
+
   properties
-    monnom ='';
-  end  %properties
-  %------
+    monnom ='';     % nom de la classe parent
+  end
+
   methods
+
     %-------------------------------
     % initial, change les properties
-    % à partir d'une structure
-    %-------
+    % Ã  partir d'une structure
+    %----------------------
     function initial(tO, V)
       CDefautFncBase.initial(tO, tO.monnom, V);
     end
-    %---------------------------------
+
+    %--------------------------------
     % databrut retourne une structure
-    % à partir des properties de tO.monnom
-    %-------
+    % Ã  partir des properties de tO.monnom
+    %-----------------------
     function V =databrut(tO)
       V =CDefautFncBase.databrut(tO, tO.monnom);
     end
+
     %-------------------------------
     % ResetProp remet les properties
-    % aux valeurs par défauts
-    %-------
+    % aux valeurs par dÃ©fauts
+    %---------------------
     function ResetProp(tO)
       CDefautFncBase.ResetProp(tO, tO.monnom);
     end
+
     %---------------------------------------------------
     % copie retourne un objet avec les properties actuel
-    %-------
+    %------------------------
     function clone =copie(tO)
       clone =CDefautFncBase.copie(tO, tO.monnom);
     end
+
   end  %methods
 end
